f25322e9dfe7975b025a77b2ef1b77603a414257
Adang Hadi Firmansyah <odymanody@gmail.com>
2024-05-02 01:23:20 +0700

    add screens

--- App.tsx
+++ App.tsx
@@ -5,61 +5,91 @@
  * @format
  */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, {useRef} from 'react';
+// import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
-  Text,
+  // Text,
   useColorScheme,
   View,
 } from 'react-native';
 
 import {
   Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
+  // DebugInstructions,
+  // Header,
+  // LearnMoreLinks,
+  // ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+import {
+  WoosmapView,
+  IndoorRendererOptions,
+  IndoorWidgetOptions,
+  // MapController,
+  // LatLngBounds,
+  // WoosPadding,
+  // LatLng,
+  // StoreOverlay,
+} from '@woosmap/react-native-woosmap';
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
+
+// function Section({children, title}: SectionProps): React.JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// }
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
+  const mapRef = useRef(null);
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  const indoorRendererConfiguration: IndoorRendererOptions = {
+    centerMap: true,
+    defaultFloor: 3,
+  };
+  const indoorWidgetConfiguration: IndoorWidgetOptions = {
+    units: 'metric',
+    ui: {
+      primaryColor: '#318276',
+      secondaryColor: '#004651',
+    },
+  };
+
+  const mapOptions = {
+    center: {lat: 40.7128, lng: -74.006}, // New York City coordinates
+    zoom: 12,
+    mapType: 'roadmap',
+    // You can add more options as needed
   };
 
   return (
@@ -68,30 +98,50 @@ function App(): React.JSX.Element {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <ScrollView
+      {/* <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+          }}> */}
+      <WoosmapView
+        ref={mapRef}
+        // mapOptions={mapOptions}
+        wooskey={'woos-a3a2b5eb-e72f-37f0-a0bf-ec51290af6fa'}
+        indoorRendererConfiguration={indoorRendererConfiguration}
+        indoorWidgetConfiguration={indoorWidgetConfiguration}
+        widget={true}
+        activateIndoorProduct={true}
+        defaultIndoorVenueKey={'mtp'}
+        loaded={() => {
+          // add();
+        }}
+        indoor_venue_loaded={info => {
+          console.log(JSON.stringify(info));
+        }}
+        indoor_level_changed={info => {
+          console.log('Level changed ' + JSON.stringify(info));
+        }}
+        indoor_feature_selected={info => {
+          console.log('Feature selected ' + JSON.stringify(info));
+        }}
+        indoor_user_location={info => {
+          console.log('User location ' + JSON.stringify(info));
+        }}
+        indoor_highlight_step={info => {
+          console.log('Step info ' + JSON.stringify(info));
+        }}
+        indoor_navigation_started={() => {
+          console.log('Navigation Started');
+        }}
+        // indoor_navigation_exited={() => {
+        //   console.log('Navigation ended');
+        //   mapRef.current?.clearDirections();
+        // }}
+      />
+      {/* </View>
+      </ScrollView> */}
     </SafeAreaView>
   );
 }
